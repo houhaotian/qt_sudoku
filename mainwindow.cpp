b2d0e279004a7328ddf130e198906032391a2644
houhaotian <haotian_h@163.com>
2018-02-27 11:35:30 +0800

    OKle

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,6 +15,10 @@
 
 ::QString str_hardclass[4] = { "简单","中等","困难","地狱" };
 int MainWindow::wrongTime = 0;
+int MainWindow::nowSelectedNum = 0;
+int MainWindow::onPressingBoard = 0;
+int MainWindow::nowGameHardLevel = 0;
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -22,9 +26,10 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->setWindowTitle(tr("我的小数独"));
-
+//  this->setAttribute(Qt::WA_TranslucentBackground, true);//设置透明2-窗体标题栏不透明,背景透明
 #if 1
     sudokuPower = new MySudoku(HardLevel::easy);
+    nowGameHardLevel = HardLevel::easy;
     MySudoku & chessB = *sudokuPower;
     QGridLayout* cboardLayout = new QGridLayout;
     QString temp;
@@ -58,7 +63,7 @@ MainWindow::MainWindow(QWidget *parent) :
         cboardLayout->setColumnStretch(i, 0);
         cboardLayout->setRowStretch(i, 0);
     }
-    ui->right->setLayout(cboardLayout);
+  //  ui->right->setLayout(cboardLayout);
 #endif
 
 
@@ -68,12 +73,25 @@ MainWindow::MainWindow(QWidget *parent) :
         hardMenu[i] = new QAction(str_hardclass[i]);
         ui->newGame->addAction(hardMenu[i]);
         connect(hardMenu[i], SIGNAL(triggered()), this, SLOT(newGameClicked()));
+        connect(this, SIGNAL(clickedBeginNewGameButton(int)), hardMenu[i], SIGNAL(triggered()));
     }
     /*菜单栏其它*/
     QAction *aboutMenu = new QAction("关于");
     ui->others->addAction(aboutMenu);
     connect(aboutMenu, SIGNAL(triggered()), this, SLOT(onAboutTriggered()));
 
+    //score添加
+    score = new QLabel(ui->left);
+    score->setObjectName(QStringLiteral("score"));
+    score->setGeometry(QRect(10, 50, 54, 21));
+    QFont font1;
+    font1.setFamily(QStringLiteral("Consolas"));
+    font1.setPointSize(11);
+    font1.setBold(true);
+    font1.setWeight(75);
+    score->setFont(font1);
+    score->setStyleSheet(QStringLiteral("color: rgb(245, 245, 245);r"));
+    score->setNum(0);//清空得分
 }
 
 MainWindow::~MainWindow()
@@ -86,8 +104,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::paintEvent(QPaintEvent *)
 {
+
     //    int bm=ui->menuBar->geometry().bottom();
-    QPainter painter(this);
+    QPainter painter(ui->right);
     QPen penType;
     penType.setWidth(3);
     penType.setColor(Qt::red);
@@ -96,7 +115,7 @@ void MainWindow::paintEvent(QPaintEvent *)
     ui->centralWidget->geometry().x()+100,ui->centralWidget->geometry().y()+100
     );
     */
-
+ painter.drawLine(1,1,500,500);
     for (int i = 3; i < 8; i += 3)
     {
         int x1 = (chess[0][i].geometry().left() + chess[0][i - 1].geometry().right()) / 2;
@@ -144,9 +163,10 @@ void MainWindow::newGameClicked()
     }
     sudokuPower = new MySudoku(incomeLevel);
     MySudoku & chessB = *sudokuPower;
-
+    nowGameHardLevel = incomeLevel;
     resetChessboard(chessB);
     nowSelectedNum = 0;//游戏重新开始
+    score->setNum(0);//清空得分
 }
 
 void MainWindow::resetChessboard(MySudoku & chessB)
@@ -237,19 +257,18 @@ void MainWindow::highLightSelectedButtons(int aimmedNum)
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
-
     QString temp(event->key());
     int n = onPressingBoard;
     int i = n / 10, j = n % 10;
     int x_t = i, y_t = j;
     vvint & chessB = sudokuPower->truth;
-    static int score = 0;
+    static int gameScore = 0;
 
     if (chessB[x_t][y_t] == temp.toInt())//如果输入正确则填入
     {
         chess[x_t][y_t].setText(temp);
-        score += 200;
-        ui->score->setNum(score);
+        gameScore += 200;
+        score->setNum(gameScore);
     }
     else
     {    //错误统计加一
@@ -270,13 +289,12 @@ void MainWindow::addOneWrong()
                 delete wrongLabel[i];
             }
             wrongTime = 0;
-            //跳到重新开始当前难度
+           emit(clickedBeginNewGameButton(nowGameHardLevel));
         }
         return;
     }
 
     wrongLabel[wrongTime] = new QLabel(ui->up);
-
     wrongLabel[wrongTime]->setGeometry(QRect(ui->label_2->x() + 50 * (wrongTime + 1), ui->label_2->y(), 40, 40));
     wrongLabel[wrongTime]->setStyleSheet(QStringLiteral("image: url(:/new/prefix1/mainico);"));
     wrongLabel[wrongTime]->setVisible(1);
--- mainwindow.h
+++ mainwindow.h
@@ -24,10 +24,8 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-
-
 signals:
-    void clickedBeginNewGameButton();
+    void clickedBeginNewGameButton(int);
 private slots:
     void onAboutTriggered();
     void newGameClicked();
@@ -35,16 +33,18 @@ private slots:
 protected:
     virtual void paintEvent(QPaintEvent *);
     virtual void keyPressEvent(QKeyEvent *event);
-
 private:
     Ui::MainWindow *ui;
     QPushButton chess[9][9];
     MySudoku *sudokuPower;//生成的数独数字
-    int nowSelectedNum;
-    int onPressingBoard;
-    static int wrongTime;
     QLabel *wrongLabel[4];
     QAction * hardMenu[4];
+    QLabel *score;
+
+    static int nowSelectedNum;
+    static int onPressingBoard;
+    static int wrongTime;
+    static int nowGameHardLevel;
 private:
     void highLightSelectedButtons(int aimmedNum);
     void resetChessboard(MySudoku &chessB);
